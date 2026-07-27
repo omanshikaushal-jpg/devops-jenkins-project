067e986cb663992a4fe9c1421948402af5013781
OmanshiKaushal <omanshi.kaushal@airbus.com>
2026-07-27 15:31:46 +0530

    Release version 2.0

--- test_app.py
+++ test_app.py
@@ -6,7 +6,7 @@ def client():
     app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
-
+        
 def test_home_page(client):
     response = client.get('/')
     assert response.status_code == 200
